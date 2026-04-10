1a41eefeb52a24751f901e40855da1121e4bbc70
zildjianvitoo <zildjianvito07@gmail.com>
2026-04-10 08:41:27 +0700

    Initial Commit

---
+++ grounded/groundedApp.swift
@@ -0,0 +1,17 @@
+//
+//  groundedApp.swift
+//  grounded
+//
+//  Created by Zildjian Vito  on 10/04/26.
+//
+
+import SwiftUI
+
+@main
+struct groundedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
